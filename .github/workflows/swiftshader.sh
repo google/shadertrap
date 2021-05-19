fd3becaf33ffa024ddc28ebfe118e25695a0f677
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2021-05-19 17:53:44 +0100

    Checkpoint.

---
+++ .github/workflows/swiftshader.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The ShaderTrap Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+set -e
+set -u
+
+
+help | head
+
+uname
+
+if [ "$(uname)" != "Linux" ]
+then
+    echo "Unsupported OS"
+    exit 1
+fi
+
+echo "deb-src http://gb.archive.ubuntu.com/ubuntu/ focal main restricted" | sudo tee -a /etc/apt/sources.list
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qy update
+
+NINJA_OS="linux"
+# Needed to get EGL
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qy install libegl1-mesa-dev
+
+# Needed to get Mesa build dependencies
+sudo DEBIAN_FRONTENT=noninteractive apt-get -qy build-dep mesa
+
+# Mesa requires Mako
+python -m pip install Mako
+
+df -h
+sudo swapoff -a
+sudo rm -f /swapfile
+sudo apt clean
+# shellcheck disable=SC2046
+docker rmi $(docker image ls -aq)
+df -h
+
+export PATH="${HOME}/depot_tools":${PATH}
+git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+
+git clone https://chromium.googlesource.com/angle/angle
+pushd "${HOME}/angle"
+  python --version
+  python scripts/bootstrap.py
+  gclient sync
+  git checkout master
+  sudo ./build/install-build-deps.sh
+  gn gen out/Release
+  autoninja -C out/Release
+popd
+
+#export SHADERTRAP_SKIP_BASH=1
+
+#source ./dev_shell.sh.template
+
+#cd ${SHADERTRAP_REPO_ROOT}
+
+#pushd temp
+#  mkdir -p "build-Debug/"
+#  pushd "build-Debug/"
+#    cmake \
+#      -G Ninja \
+#      ../.. \
+#      -DCMAKE_BUILD_TYPE=Debug \
+#      -DSHADERTRAP_USE_LLVM_LIBCPP=1
+#    cmake --build . --config Debug
+#  popd
+#popd
+
+#for f in `find ./examples -name "*.shadertrap"`
+#do
+#    env LD_LIBRARY_PATH=${HOME}/mesa/mesa-21.1.0/dynamiclibs ./temp/build-Debug/src/shadertrap/shadertrap $f --require-vendor-renderer-substring llvmpipe
+#done
